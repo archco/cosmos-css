c3a8bc2f1af7a92fdea5def609fc4bcf0e87480e
archco <archcoster@gmail.com>
2016-10-27 11:42:38 +0900

    Modify style.scss
    style.scss 의 내부 구조 변경.
    variable, helper, scaffolding, customize 로 분할.

--- gulpfile.js
+++ gulpfile.js
@@ -3,33 +3,33 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var pug = require('gulp-pug');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default', ['sass', 'pug']);
 
-gulp.task('watch', function () {
-	gulp.watch('./src/sass/**/*.scss', ['sass']);
-	gulp.watch('./tests/views/**/*.pug', ['pug']);
-});
+gulp.task('watch', ['sass:watch', 'pug:watch']);
 
 gulp.task('sass', function () {
-	return gulp.src('./src/sass/style.scss')
+	return gulp.src('src/sass/style.scss')
+		.pipe(sourcemaps.init())
 		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./dist/css/'));
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest('dist/css/'));
 });
 
 gulp.task('sass:watch', function () {
-	gulp.watch('./src/sass/**/*.scss', ['sass']);
+	gulp.watch('src/sass/**/*.scss', ['sass']);
 });
 
 gulp.task('pug', function () {
-	return gulp.src('./tests/views/*.pug')
+	return gulp.src('tests/views/*.pug')
 		.pipe(pug({
 			// options.
 			pretty: true,
 		}))
-		.pipe(gulp.dest('./tests/'));
+		.pipe(gulp.dest('tests/'));
 });
 
 gulp.task('pug:watch', function () {
-	gulp.watch('./tests/views/**/*.pug', ['pug']);
+	gulp.watch('tests/views/**/*.pug', ['pug']);
 });
