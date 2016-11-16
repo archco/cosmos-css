263fd79c153de52046cd001a1bda6a1bbe051038
archco <archcoster@gmail.com>
2016-11-16 18:28:29 +0900

    Add sass component "ajax-loading" #17

--- dist/js/script.js
+++ dist/js/script.js
@@ -346,6 +346,38 @@ function parallaxProcess(p) {
 	// set parallax height.
 	p.style.height = h + 'px';
 }
+'use strict';
+
+/************************************************************
+	AJAX-loading
+*************************************************************
+*	
+*/
+(function () {
+	var a = document.querySelector('#ajax-loading');
+	if (!a) {
+		return;
+	}
+
+	// append loader element.
+	appendLoader(a);
+	// Register callback to jquery ajax.
+	$(document).ajaxStart(function () {
+		if (!a.classList.contains('show')) {
+			a.classList.add('show');
+		}
+	}).ajaxStop(function () {
+		if (a.classList.contains('show')) {
+			a.classList.remove('show');
+		}
+	});
+
+	function appendLoader(a) {
+		var loader = document.createElement('div');
+		loader.classList.add('loader');
+		a.appendChild(loader);
+	}
+})();
 // Just example
 // console.log("Hello, World!");
 "use strict";
---
+++ src/js/cosmos_modules/ajax-loading.js
@@ -0,0 +1,30 @@
+/************************************************************
+	AJAX-loading
+*************************************************************
+*	
+*/
+(function () {
+	var a = document.querySelector('#ajax-loading');
+	if (! a) { return; }
+
+	// append loader element.
+	appendLoader(a);
+	// Register callback to jquery ajax.
+	$(document)
+		.ajaxStart(function () {
+			if (! a.classList.contains('show')) {
+				a.classList.add('show');
+			}
+		})
+		.ajaxStop(function () {
+			if (a.classList.contains('show')) {
+				a.classList.remove('show');
+			}
+		});
+
+	function appendLoader(a) {
+		var loader = document.createElement('div');
+		loader.classList.add('loader');
+		a.appendChild(loader);
+	}
+})();
