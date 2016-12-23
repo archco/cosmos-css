a31edd4bdf72aa21ceef9fba8bfd6ac3f8a9f65c
archco <archcoster@gmail.com>
2016-12-23 12:28:33 +0900

    Improve color.js #69
    add Color.contrast() method

--- dist/js/script.js
+++ dist/js/script.js
@@ -95,78 +95,142 @@ var Util = function () {
 }();
 'use strict';
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
 /************************************************************
   Color
 *************************************************************/
 var Color = function () {
   var NAME = 'Cosmos.Color';
-
-  // public
-
-  /**
-   * hexToRgb
-   * @link http://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
-   * @param  string  hex
-   * @return array|null
-   */
-  var hexToRgb = function hexToRgb(hex) {
-    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-    hex = hex.replace(shorthandRegex, function (m, r, g, b) {
-      return r + r + g + g + b + b;
-    });
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result ? [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)] : null;
+  var Config = {
+    lightnessPoint: 140,
+    darkDefault: '#000000',
+    lightDefault: '#FFFFFF'
   };
 
-  /**
-   * rgb values to hex color string
-   * 
-   * @param  number r
-   * @param  number g
-   * @param  number b
-   * @return string
-   */
-  var rgbToHex = function rgbToHex(r, g, b) {
-    r = r.toString(16);
-    g = g.toString(16);
-    b = b.toString(16);
-    return "#" + r + g + b;
-  };
+  var Color = function () {
+    function Color(color) {
+      _classCallCheck(this, Color);
 
-  /**
-   * get rgb color's lightness value.
-   * 
-   * @param  string|array  color
-   * @return number  (0 ~ 255)
-   */
-  var lightness = function lightness(color) {
-    var r = void 0,
-        g = void 0,
-        b = void 0;
-    if (typeof color == 'string') {
-      var rgb = hexToRgb(color);
-      r = rgb[0];
-      g = rgb[1];
-      b = rgb[2];
-    } else if (Array.isArray(color)) {
-      r = color[0];
-      g = color[1];
-      b = color[2];
-    } else {
-      throw new Error('parameter only "hex color" or "rgb array"');
+      this._color = Color.colorToArray(color);
     }
-    // Color lightness formula.
-    // @link https://www.w3.org/TR/AERT#color-contrast
-    return (r * 299 + g * 587 + b * 114) / 1000;
-  };
 
-  return {
-    name: NAME,
-    hexToRgb: hexToRgb,
-    rgbToHex: rgbToHex,
-    lightness: lightness
-  };
+    _createClass(Color, [{
+      key: 'getContrast',
+      value: function getContrast(dark, light) {
+        return Color.contrast(this._color, dark, light);
+      }
+
+      // static methods
+
+    }], [{
+      key: 'colorToArray',
+
+
+      /**
+       * color to rgb array
+       * @param  string|array color
+       * @return array  [red, green, blue]
+       */
+      value: function colorToArray(color) {
+        var array = [];
+        if (typeof color == 'string') {
+          array = Color.hexToRgb(color);
+        } else if (Array.isArray(color)) {
+          array = color;
+        } else {
+          throw new Error('parameter only "hex color" or "rgb array"');
+        }
+        return array;
+      }
+
+      /**
+       * hexToRgb
+       * @link http://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+       * @param  string  hex
+       * @return array|null
+       */
+
+    }, {
+      key: 'hexToRgb',
+      value: function hexToRgb(hex) {
+        // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+        var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+        hex = hex.replace(shorthandRegex, function (m, r, g, b) {
+          return r + r + g + g + b + b;
+        });
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return result ? [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)] : null;
+      }
+
+      /**
+       * rgb values to hex color string
+       * 
+       * @param  number r
+       * @param  number g
+       * @param  number b
+       * @return string
+       */
+
+    }, {
+      key: 'rgbToHex',
+      value: function rgbToHex(r, g, b) {
+        r = r.toString(16);
+        g = g.toString(16);
+        b = b.toString(16);
+        return "#" + r + g + b;
+      }
+    }, {
+      key: 'lightness',
+
+
+      /**
+       * get rgb color's lightness value.
+       * 
+       * @param  string|array  color
+       * @return number  (0 ~ 255)
+       */
+      value: function lightness(color) {
+        var rgb = Color.colorToArray(color);
+        // Color lightness formula.
+        // @link https://www.w3.org/TR/AERT#color-contrast
+        return (rgb[0] * 299 + rgb[1] * 587 + rgb[2] * 114) / 1000;
+      }
+    }, {
+      key: 'contrast',
+
+
+      /**
+       * get contrast color
+       * @param  array|string color
+       * @param  string  dark
+       * @param  string  light
+       * @return string  dark or light
+       */
+      value: function contrast(color) {
+        var dark = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : Config.darkDefault;
+        var light = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : Config.lightDefault;
+
+        var lightness = Color.lightness(color);
+        if (lightness > Config.lightnessPoint) {
+          return dark;
+        } else {
+          return light;
+        }
+      }
+    }, {
+      key: 'name',
+      get: function get() {
+        return NAME;
+      }
+    }]);
+
+    return Color;
+  }();
+
+  return Color;
 }();
 'use strict';
 
