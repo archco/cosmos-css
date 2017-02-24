e844ea6272698f9d4521ae2dea0ea6adc21e9261
archco <archcoster@gmail.com>
2017-02-24 11:36:26 +0900

    Change "src/" directory structure #107

--- src/app/js/modules/app.js
+++
@@ -1,2 +0,0 @@
-// Just example
-// console.log("Hello, World!");
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/cosmos/js/load.js',
+  entry: './src/js/load.js',
   output: {
     filename: 'script.js',
     path: path.resolve(__dirname, 'dist/js/'),
